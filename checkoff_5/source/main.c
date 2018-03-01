4ac331b5000f22f55a1c0388aa1c3360b5f7d742
charleneeshong@berkeley.edu <charleneeshong@berkeley.edu>
2018-03-01 14:03:03 -0800

    moved adc demo code into main.c

--- checkoff_5/source/main.c
+++ checkoff_5/source/main.c
@@ -41,9 +41,10 @@
 #include "fsl_debug_console.h"
 #include "board.h"
 #include "fsl_ftm.h"
+#include "fsl_adc16.h"
 
-#include "pin_mux.h"
 #include "clock_config.h"
+#include "pin_mux.h"
 
 /*******************************************************************************
  * Definitions
@@ -77,6 +78,18 @@
 //High Voltage(3.3V)=True for pwm
 #define PWM_LEVEL kFTM_HighTrue
 
+/* Checkoff 5 */
+
+#define DEMO_ADC16_BASEADDR ADC0
+#define DEMO_ADC16_CHANNEL_GROUP 0U
+#define DEMO_ADC16_USER_CHANNEL 12U /* PTB2, ADC0_SE12 */
+
+#define DEMO_ADC16_IRQn ADC0_IRQn
+#define DEMO_ADC16_IRQ_HANDLER_FUNC ADC0_IRQHandler
+
+#define VREF_BRD 3.300
+#define SE_12BIT 4096.0
+
 /*******************************************************************************
  * Prototypes
  ******************************************************************************/
@@ -87,17 +100,26 @@ void delay(void);
 void update_duty_cycle(uint8_t updated_duty_cycle);
 void init_pwm(uint32_t freq_hz, uint8_t init_duty_cycle);
 
+/* Initialize ADC16 */
+static void ADC_Init(void);
+static void init_board(void);
+void read_ADC(void);
+
 /*******************************************************************************
  * Variables
  ******************************************************************************/
 volatile bool ftmIsrFlag = false;
 volatile uint8_t duty_cycle = 1U;
 
-const int SERVO_DUTY_MIN = 0;
+const int SERVO_DUTY_MIN = 0; //
 const int SERVO_DUTY_MAX = 40;
 
 //const int SERVO_DUTY_MIN = 5;
 //const int SERVO_DUTY_MAX = 50;
+
+volatile bool g_Adc16ConversionDoneFlag = false;
+volatile uint32_t g_Adc16ConversionValue = 0;
+adc16_channel_config_t g_adc16ChannelConfigStruct;
 
 /*******************************************************************************
  * Code
@@ -172,6 +194,74 @@ void update_duty_cycle(uint8_t updated_duty_cycle)
 	FTM_EnableInterrupts(BOARD_FTM_BASEADDR, FTM_CHANNEL_INTERRUPT_ENABLE);
 }
 
+static void ADC_Init(void)
+{
+	EnableIRQ(DEMO_ADC16_IRQn);
+    adc16_config_t adc16ConfigStruct;
+
+    /* Configure the ADC16. */
+    /*
+     * adc16ConfigStruct.referenceVoltageSource = kADC16_ReferenceVoltageSourceVref;
+     * adc16ConfigStruct.clockSource = kADC16_ClockSourceAsynchronousClock;
+     * adc16ConfigStruct.enableAsynchronousClock = true;
+     * adc16ConfigStruct.clockDivider = kADC16_ClockDivider8;
+     * adc16ConfigStruct.resolution = kADC16_ResolutionSE12Bit;
+     * adc16ConfigStruct.longSampleMode = kADC16_LongSampleDisabled;
+     * adc16ConfigStruct.enableHighSpeed = false;
+     * adc16ConfigStruct.enableLowPower = false;
+     * adc16ConfigStruct.enableContinuousConversion = false;
+     */
+
+    ADC16_GetDefaultConfig(&adc16ConfigStruct);
+#if defined(BOARD_ADC_USE_ALT_VREF)
+    adc16ConfigStruct.referenceVoltageSource = kADC16_ReferenceVoltageSourceValt;
+#endif
+    ADC16_Init(DEMO_ADC16_BASEADDR, &adc16ConfigStruct);
+
+    /* Make sure the software trigger is used. */
+    ADC16_EnableHardwareTrigger(DEMO_ADC16_BASEADDR, false);
+#if defined(FSL_FEATURE_ADC16_HAS_CALIBRATION) && FSL_FEATURE_ADC16_HAS_CALIBRATION
+    if (kStatus_Success == ADC16_DoAutoCalibration(DEMO_ADC16_BASEADDR))
+    {
+        PRINTF("\r\nADC16_DoAutoCalibration() Done.");
+    }
+    else
+    {
+        PRINTF("ADC16_DoAutoCalibration() Failed.\r\n");
+    }
+#endif /* FSL_FEATURE_ADC16_HAS_CALIBRATION */
+
+    /* Prepare ADC channel setting */
+    g_adc16ChannelConfigStruct.channelNumber = DEMO_ADC16_USER_CHANNEL;
+    g_adc16ChannelConfigStruct.enableInterruptOnConversionCompleted = true;
+
+#if defined(FSL_FEATURE_ADC16_HAS_DIFF_MODE) && FSL_FEATURE_ADC16_HAS_DIFF_MODE
+    g_adc16ChannelConfigStruct.enableDifferentialConversion = false;
+#endif /* FSL_FEATURE_ADC16_HAS_DIFF_MODE */
+}
+
+void read_ADC(void){
+	g_Adc16ConversionDoneFlag = false;
+	ADC16_SetChannelConfig(DEMO_ADC16_BASEADDR, DEMO_ADC16_CHANNEL_GROUP, &g_adc16ChannelConfigStruct);
+
+	//Block until the ADC Conversion is finished
+	 while (!g_Adc16ConversionDoneFlag)
+	 {
+	 }
+}
+
+void DEMO_ADC16_IRQ_HANDLER_FUNC(void)
+{
+    g_Adc16ConversionDoneFlag = true;
+    /* Read conversion result to clear the conversion completed flag. */
+    g_Adc16ConversionValue = ADC16_GetChannelConversionValue(DEMO_ADC16_BASEADDR, DEMO_ADC16_CHANNEL_GROUP);
+    /* Add for ARM errata 838869, affects Cortex-M4, Cortex-M4F Store immediate overlapping
+      exception return operation might vector to incorrect interrupt */
+#if defined __CORTEX_M && (__CORTEX_M == 4U)
+    __DSB();
+#endif
+}
+
 /*!
  * @brief Main function
  */
@@ -179,10 +269,14 @@ int main(void)
 
 {
 	init_board();
-	init_pwm(10000, 0); //start 20khz pwm at 40% duty cycle
+	ADC_Init();
+	float analog_voltage;
+
+	init_pwm(10000, 0); //start 10khz pwm at 0% duty cycle
 	int duty_cycle = 0;
-//	init_pwm(500, 40); //start 20khz pwm at 40% duty cycle
+//	init_pwm(500, 40); //start 500hz pwm at 40% duty cycle
 //	int duty_cycle = 25;
+
 	while (1) {
 		char ch = GETCHAR(); //read from serial terminal
 		if (ch == 'a') { //turn left
@@ -190,16 +284,20 @@ int main(void)
 				duty_cycle = SERVO_DUTY_MIN;
 			} else {
 				duty_cycle -= 2;
-				update_duty_cycle(duty_cycle); //subtract 1% from duty cycle
+				update_duty_cycle(duty_cycle); //subtract 2% from duty cycle
 			}
 		} else if (ch == 'd') { //turn right
-			if (duty_cycle + 2 > SERVO_DUTY_MAX) { //case where duty cycle exceeds 50%
+			if (duty_cycle + 2 > SERVO_DUTY_MAX) { //case where duty cycle exceeds 40%
 				duty_cycle = SERVO_DUTY_MAX;
 			} else {
 				duty_cycle += 2;
-				update_duty_cycle(duty_cycle); //add 1% to duty cycle
+				update_duty_cycle(duty_cycle); //add 2% to duty cycle
 			}
 		}
+
+        read_ADC();
+        analog_voltage = (float)(g_Adc16ConversionValue * (VREF_BRD / SE_12BIT));
+        PRINTF("\r\n\r\nADC Value: %d, ADC Voltage: %0.3f\r\n", g_Adc16ConversionValue, analog_voltage);
 	}
 }
 
